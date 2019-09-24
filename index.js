98c2630fa9bcf64c9472ebb50cf9c40a43a4e9a7
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-09-24 19:47:11 +1000

    Live ISS tracking working. Mapping has a bug of duplicating marker.

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const endpoint = "https://api.wheretheiss.at/v1/satellites"
 const createElem = (elem) => document.createElement(elem)
 const appendNode =  (parent, node) => parent.appendChild(node)
 
+const title = createElem('h1')
+title.innerHTML = 'Where is the International Space Station?'
+const titleSectionWrapper = document.getElementById('title-section-wrapper')
+appendNode(titleSectionWrapper, title)
+const longitudeText = createElem('h3')
+appendNode(titleSectionWrapper, longitudeText)
+const latitudeText = createElem('h3')
+appendNode(titleSectionWrapper, latitudeText)
+
 const getISS = async () => {
   const resp = await fetch(endpoint)
   const data = await resp.json()
@@ -11,67 +20,48 @@ const getISS = async () => {
   return spaceStationURL
 }
 
-getISS()
-
-const getISSLocation = async () => {
-  const iss = await getISS()
-  const resp = await fetch(iss)
-  const data = await resp.json()
-  const issLatitude = data.latitude
-  const issLongitude = data.longitude
-  console.log(issLongitude, issLatitude)
-  return data
-}
-
-getISSLocation()
-
-const issLongitude = async () => {
-  const location = await getISSLocation()
-  const longitude = location.longitude
-  const longitudeText = createElem('h3')
+const getISSData = async () => {  
+  const apiEndpoint = await getISS()
+  console.log(apiEndpoint)
+  const response = await fetch(apiEndpoint)
+  const data = await response.json()
+  const {latitude, longitude} =  data;
+  
+  console.log(latitude, longitude)
+  
   longitudeText.innerHTML = `${longitude}`
-  appendNode(titleSectionWrapper, longitudeText)
-}
-
-const issLatitude = async () => {
-  const location = await getISSLocation()
-  const latitude = location.latitude
-  const latitudeText = createElem('h3')
   latitudeText.innerHTML = `${latitude}`
-  appendNode(titleSectionWrapper, latitudeText)
+  
+  map.setView([longitude, latitude], 4)
+  let marker = markerr
+  
+  updateISSMarker(latitude, longitude, marker)
 }
 
-console.log(issLongitude(), issLatitude())
+const updateISSMarker = (lat, long, marker) => {
+  marker.latitude = lat
+  marker.longitude = long
+}
 
-const title = createElem('h1')
-title.innerHTML = 'Where is the International Space Station?'
+const issIcon = L.icon({
+  iconUrl: './assets/images/satellite-icon.svg',
+  iconSize: [20, 20],
+  iconAnchor: [25, 16]
+})
 
-const titleSectionWrapper = document.getElementById('title-section-wrapper')
-appendNode(titleSectionWrapper, title)
+const map = L.map('mapid').setView([0, 0], 0)
+const markerr = L.marker([0, 0], {icon: issIcon}).addTo(map)
 
+const showMap = () => {
+  const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-const map = L.map('mapid').setView([0, 0], 1);
-  // setView(lat, long, and zoom level)
+  const tiles = L.tileLayer(tileUrl, {attribution})
+  tiles.addTo(map)
+}
 
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileURL, {attribution})
-tiles.addTo(map)
+showMap();
 
-const marker = L.marker([0, 0]).addTo(map);
-
-
-// From docs
-let myIcon = L.icon({
-  iconUrl: '',
-  iconSize: [38, 95],
-  iconAnchor: [22, 94],
-  popupAnchor: [-3, -76],
-  shadowUrl: '',
-  shadowSize: [68, 95],
-  shadowAnchor: [22, 94]
-});
-
-setInterval(function() {
-  L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
-}, 2000)
+setInterval(() => {
+  getISSData()
+}, 2500)
